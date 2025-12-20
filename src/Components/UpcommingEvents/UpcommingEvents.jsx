2f69db325b0e5f9fec120a409b0196d7262a1595
MD. Owasiul Islam <sanisuper0@gmail.com>
2025-12-20 06:42:52 +0600

    added px-3 for more fluid screning for 1024px

--- src/Components/UpcommingEvents/UpcommingEvents.jsx
+++ src/Components/UpcommingEvents/UpcommingEvents.jsx
@@ -7,7 +7,7 @@ const UpcommingEvents = ({ futureEvents }) => {
     <div className="mt-4 w-[90%] mx-auto">
       <h2 className="md:text-3xl text-2xl font-bold">Upcomming Events</h2>
 
-      <div className="up1024 mt-5 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center items-center">
+      <div className="up1024 px-3 mt-5 grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 justify-center items-center">
         {futureEvents.map((events, id) => (
           <EventsCards key={id} events={events} />
         ))}
